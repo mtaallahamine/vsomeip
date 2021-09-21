923ec7012b81664f993c04a10616d94126a16bde
Hakim-Git <missawihakim@gmail.com>
2021-09-21 17:21:49 +0100

    new files getter setter updates

--- examples/getter_setter/notify-service.cpp
+++ examples/getter_setter/notify-service.cpp
@@ -11,32 +11,7 @@
 #define SAMPLE_EVENT_ID 0x8778
 
 std::shared_ptr<vsomeip::application> app;
-std::shared_ptr<vsomeip::payload> payload_notif;
-const vsomeip::byte_t its_data[] = { 0x10 };
 
- /*   void on_get(const std::shared_ptr<vsomeip::message> &_message) {
-        std::cout << "*************Server Notifier :: entry on_get method *************" << std::endl;
-        std::shared_ptr<vsomeip::message> its_response
-            = vsomeip::runtime::get()->create_response(_message);
-        {
-            its_response->set_payload(payload_);
-        }
-        app->send(its_response);
-    }
-
-    void on_set(const std::shared_ptr<vsomeip::message> &_message) {
-        std::cout << "*************Server Notifier :: entry on_set method *************" << std::endl;
-        std::shared_ptr<vsomeip::message> its_response
-            = vsomeip::runtime::get()->create_response(_message);
-        {
-            payload_ = _message->get_payload();
-            its_response->set_payload(payload_);
-        }
-
-        app->send(its_response);
-        app->notify(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID,
-                     SAMPLE_EVENT_ID, payload_);
-    }*/
 void on_message(const std::shared_ptr<vsomeip::message> &_request) {
 
     std::shared_ptr<vsomeip::payload> its_payload = _request->get_payload();
@@ -66,13 +41,14 @@ void on_message(const std::shared_ptr<vsomeip::message> &_request) {
     app->send(its_response);
 }
 int main(){
+const vsomeip::byte_t its_data[] = { 0x10 };
+std::shared_ptr<vsomeip::payload> payload_notif;
 payload_notif = vsomeip::runtime::get()->create_payload();
 payload_notif->set_data(its_data, sizeof(its_data));
 
 app = vsomeip::runtime::get()->create_application("notify-service");
 app->init();
 app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_message);
-//app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_set);
 std::set<vsomeip::eventgroup_t> its_groups;
 its_groups.insert(SAMPLE_EVENTGROUP_ID);
 app->offer_event(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENT_ID, its_groups, vsomeip::event_type_e::ET_FIELD);
