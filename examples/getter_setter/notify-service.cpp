5f756a3498e747dfa80dfbcf07df33b04b153c04
Hakim-Git <missawihakim@gmail.com>
2021-09-21 15:19:51 +0100

    update notify.cpp

--- examples/getter_setter/notify-service.cpp
+++ examples/getter_setter/notify-service.cpp
@@ -11,9 +11,10 @@
 #define SAMPLE_EVENT_ID 0x8778
 
 std::shared_ptr<vsomeip::application> app;
-std::shared_ptr<vsomeip::payload> payload_;
+std::shared_ptr<vsomeip::payload> payload_notif;
+const vsomeip::byte_t its_data[] = { 0x10 };
 
-    void on_get(const std::shared_ptr<vsomeip::message> &_message) {
+ /*   void on_get(const std::shared_ptr<vsomeip::message> &_message) {
         std::cout << "*************Server Notifier :: entry on_get method *************" << std::endl;
         std::shared_ptr<vsomeip::message> its_response
             = vsomeip::runtime::get()->create_response(_message);
@@ -35,22 +36,47 @@ std::shared_ptr<vsomeip::payload> payload_;
         app->send(its_response);
         app->notify(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID,
                      SAMPLE_EVENT_ID, payload_);
+    }*/
+void on_message(const std::shared_ptr<vsomeip::message> &_request) {
+
+    std::shared_ptr<vsomeip::payload> its_payload = _request->get_payload();
+    vsomeip::length_t l = its_payload->get_length();
+
+    // Get payload
+    std::stringstream ss;
+    for (vsomeip::length_t i=0; i<l; i++) {
+       ss << std::setw(2) << std::setfill('0') << std::hex
+          << (int)*(its_payload->get_data()+i) << " ";
     }
 
-int main(){
-const vsomeip::byte_t its_data[] = { 0x10 };
-payload_ = vsomeip::runtime::get()->create_payload();
-payload_->set_data(its_data, sizeof(its_data));
+    std::cout << "SERVICE: Received message with Client/Session ["
+        << std::setw(4) << std::setfill('0') << std::hex << _request->get_client() << "/"
+        << std::setw(4) << std::setfill('0') << std::hex << _request->get_session() << "] "
+        << ss.str() << std::endl;
 
+    // Create response
+    std::shared_ptr<vsomeip::message> its_response = vsomeip::runtime::get()->create_response(_request);
+    its_payload = vsomeip::runtime::get()->create_payload();
+    std::vector<vsomeip::byte_t> its_payload_data;
+    for (int i=9; i>=0; i--) {
+        its_payload_data.push_back(i % 256);
+    }
+    its_payload->set_data(its_payload_data);
+    its_response->set_payload(its_payload);
+    app->send(its_response);
+}
+int main(){
+payload_notif = vsomeip::runtime::get()->create_payload();
+payload_notif->set_data(its_data, sizeof(its_data));
 
 app = vsomeip::runtime::get()->create_application("notify-service");
 app->init();
-app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_get);
-app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_set);
+app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_message);
+//app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_set);
 std::set<vsomeip::eventgroup_t> its_groups;
 its_groups.insert(SAMPLE_EVENTGROUP_ID);
 app->offer_event(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENT_ID, its_groups, vsomeip::event_type_e::ET_FIELD);
-app->notify(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENT_ID, payload_);
+app->notify(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENT_ID, payload_notif);
 app->start();
 
 }
